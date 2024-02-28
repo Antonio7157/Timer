b22acd64cca0f92c7a32c39e3ceeee13888a50c8
Antonio7157 <antonioodhiambo50@gmail.com>
2024-02-28 16:21:18 +0300

    Validate input

--- script.js
+++ script.js
@@ -19,3 +19,21 @@ setInterval(function() {
         session.innerHTML = "PM";
     }
 }, 1000);
+
+let hoursTimer = 0;
+let minutesTimer = 0;
+let secondsTimer = 0;
+let timerInterval;
+
+function startTimer() {
+    hoursTimer = parseInt(document.getElementById("hours-input").value) || 0;
+    minutesTimer = parseInt(document.getElementById("minutes-input").value) || 0;
+    secondsTimer = parseInt(document.getElementById("seconds-input").value) || 0;
+    
+
+    if (hoursTimer < 0 || hoursTimer > 23 || minutesTimer < 0 || minutesTimer > 59 || secondsTimer < 0 || secondsTimer > 59) {
+        alert("Invalid time input!");
+        return;
+    }
+}
+ 
